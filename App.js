185b7cc07bd500d52b494f07757ed9c606778422
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-20 11:25:24 -0700

    asset updates

--- App.js
+++ App.js
@@ -4,14 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import Home from './screens/Home';
+import Details from './screens/Details';
+import { useFonts } from 'expo-font';
 
 const Stack = createStackNavigator();
- const theme= {
-  ...DefaultTheme.colors,
-  background:"transparent"
- }
+
+const theme = {
+  colors:{
+  
+    ...DefaultTheme.colors,
+    background: "transparent",
+  }}
 
 const App= ()=> {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+
+  });
   return (
     <NavigationContainer theme={theme}>
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Text } from 'react-native'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <Text>Home</Text>
   )
 }
 
